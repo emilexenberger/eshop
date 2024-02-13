e198e9097ca9864ce8a065b21beb0680f56d58a8
emil.exenberger <emil.exenberger@gmail.com>
2024-02-13 16:31:03 +0100

    Changed to local DB

--- src/main/java/webemex/eshop/EshopApplication.java
+++ src/main/java/webemex/eshop/EshopApplication.java
@@ -7,9 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = {"webemex.eshop.repository"})
 public class EshopApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EshopApplication.class, args);
 	}
-
 }
