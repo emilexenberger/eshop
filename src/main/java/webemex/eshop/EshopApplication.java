06f752b94513f2046439723ef11f14a720ad126f
emil.exenberger <emil.exenberger@gmail.com>
2024-02-19 18:01:46 +0100

    Removed unecessary lines

--- src/main/java/webemex/eshop/EshopApplication.java
+++ src/main/java/webemex/eshop/EshopApplication.java
@@ -1,13 +1,9 @@
 package webemex.eshop;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@EnableJpaRepositories(basePackages = {"webemex.eshop.repository"})
 public class EshopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EshopApplication.class, args);
